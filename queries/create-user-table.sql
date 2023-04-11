fc962f9560fcbe68b1c7a869d22a8c56cea489d6
Emily Daitch <95369494+emily-daitch@users.noreply.github.com>
2023-04-10 21:53:47 -0400

    Create create-user-table.sql

---
+++ queries/create-user-table.sql
@@ -0,0 +1,7 @@
+// for strava user testing
+create table `users` ( 
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  `username` varchar(100) NOT NULL, 
+  `email` varchar(100) NOT NULL, 
+  `password` varchar(100) NOT NULL 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
